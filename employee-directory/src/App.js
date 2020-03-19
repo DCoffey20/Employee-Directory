f0833f7597f07adde1c0ee6961ea797103602891
Dylan Coffey <dcoffey20@gmail.com>
2020-03-18 22:00:33 -0500

    created the API call and inserted the search Bar

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from "./components/Header";
 import Card from "./components/Card/Card";
+import SearchForm from "./components/SearchForm";
 import Employee from "./components/Employee";
 import './App.css';
 import API from "./util/API";
 
 
+class App extends Component {
+  employeeAPI = query => {
+    API.search(query)
+      .then(res => this.setState({ result: res.data }))
+      .catch(err => console.log(err));
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Card />
+  componentDidMount() {
+    this.employeeAPI();
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header />
+        <SearchForm />
+        <Card />
+
+      </div>
+    );
+  }
 }
 
 export default App;
